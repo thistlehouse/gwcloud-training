66b0c7f9a192d6e21f7e9ff778a282d918daca95
thistlehouse <fernando2802@gmail.com>
2023-04-09 20:05:22 -0300

    added TestProject4 from part 3

---
+++ MicrosofContent/CsharpProjects/Part3/TestProject4/Program.cs
@@ -0,0 +1,20 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 1) Console.WriteLine(i);
+    else if (i % 3 == 2) Console.WriteLine(i);
+    else
+    {
+        if (i % 3 == 0)
+        {
+            if (i % 5 == 0)
+                Console.WriteLine($"{i} - FizzBuzz");
+            else
+                Console.WriteLine($"{i} - Fizz");
+        } 
+        else
+        {
+            if (i % 3 == 0) 
+                Console.WriteLine($"{i} - Fizz");
+        }
+    }    
+}
