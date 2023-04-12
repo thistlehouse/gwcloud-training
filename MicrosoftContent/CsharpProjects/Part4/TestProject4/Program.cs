ab4a12082875bfdc33b7453789d76402c745dd7b
thistlehouse <fernando2802@gmail.com>
2023-04-12 14:50:24 -0300

    added TestProject4 from part 4

---
+++ MicrosoftContent/CsharpProjects/Part4/TestProject4/Program.cs
@@ -0,0 +1,30 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+string spanOpenTag = "<span>";
+string spanCloseTag = "</span>";
+string divOpenTag = "<div>";
+string divCloseTag = "</div>";
+
+int openSpan = input.IndexOf(spanOpenTag);
+int closeSpan = input.IndexOf(spanCloseTag);
+int openDiv = input.IndexOf(divOpenTag);
+
+openSpan += spanOpenTag.Length;
+
+int spanLength = (closeSpan - openSpan);
+
+quantity = input.Substring(openSpan, spanLength);
+
+output = input.Replace("&trade;", "&reg;");
+output = output.Remove(openDiv, divOpenTag.Length);
+
+int closeDiv = output.IndexOf(divCloseTag);
+
+output = output.Remove(closeDiv, divCloseTag.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
