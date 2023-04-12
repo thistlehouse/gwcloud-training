7fd2f523bb15ea81dd4b788f31e29dab9d3a95e5
thistlehouse <fernando2802@gmail.com>
2023-04-12 10:30:32 -0300

    added TestProject2 from part 4

---
+++ MicrosoftContent/CsharpProjects/Part4/TestProject2/Program.cs
@@ -0,0 +1,17 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+char[] pangramChar = pangram.ToCharArray();
+
+Array.Reverse(pangramChar);
+
+string newPangram = new string(pangramChar);
+
+string[] newPangramWords = newPangram.Split(" ");
+
+Array.Reverse(newPangramWords);
+
+string result = String.Join(" ", newPangramWords);
+
+Console.WriteLine(result);
+
+Console.ReadLine();
