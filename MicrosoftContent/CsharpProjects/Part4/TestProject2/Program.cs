f96f4bd6d5cdfb59b761a4b5e32516ab90f93bb6
thistlehouse <fernando2802@gmail.com>
2023-04-12 10:37:03 -0300

    added challenge to TestProject2 from part 4

--- MicrosoftContent/CsharpProjects/Part4/TestProject2/Program.cs
+++ MicrosoftContent/CsharpProjects/Part4/TestProject2/Program.cs
@@ -1,17 +1,35 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
+﻿// string pangram = "The quick brown fox jumps over the lazy dog";
 
-char[] pangramChar = pangram.ToCharArray();
+// char[] pangramChar = pangram.ToCharArray();
 
-Array.Reverse(pangramChar);
+// Array.Reverse(pangramChar);
 
-string newPangram = new string(pangramChar);
+// string newPangram = new string(pangramChar);
 
-string[] newPangramWords = newPangram.Split(" ");
+// string[] newPangramWords = newPangram.Split(" ");
 
-Array.Reverse(newPangramWords);
+// Array.Reverse(newPangramWords);
 
-string result = String.Join(" ", newPangramWords);
+// string result = String.Join(" ", newPangramWords);
 
-Console.WriteLine(result);
+// Console.WriteLine(result);
 
-Console.ReadLine();
+// Console.ReadLine();
+
+/*
+
+*/
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderStreamArray = orderStream.Split(",");
+
+foreach(string order in orderStreamArray)
+{
+    char[] orderId = order.ToCharArray();
+
+    if (orderId.Length == 4)
+        Console.WriteLine(order);
+    else
+        Console.WriteLine(order + "\t- Error");
+}
