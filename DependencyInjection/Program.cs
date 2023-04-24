8cf1e678e4e387c1fb22a420278a6da75ccde550
thistlehouse <fernando2802@gmail.com>
2023-04-24 16:16:29 -0300

    using host builder

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -6,37 +6,17 @@ using DI.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var serviceProvider = new ServiceCollection()
-    .AddSingleton<ICalculatorService, CalculatorService>()
-    .AddSingleton<IDisplayMenuService, DisplayMenuService>()
-    .AddSingleton<ICalculator, Calculator>()
-    .AddSingleton<IInputReaderService, InputReaderService>()
-    .AddScoped<IMathematicOperation, MathematicOperation>()
-    .BuildServiceProvider();
+var builder =  Host.CreateDefaultBuilder(args);
 
-var calculator = serviceProvider.GetService<ICalculator>();
+builder.ConfigureServices(services => 
+    services.AddHostedService<Calculator>()
+        .AddSingleton<IDisplayMenuService, DisplayMenuService>()
+        .AddScoped<ICalculatorService, CalculatorService>()
+        .AddScoped<ICalculator, Calculator>()
+        .AddScoped<IInputReaderService, InputReaderService>()
+        .AddScoped<IMathematicOperation, MathematicOperation>()
+    );
 
-calculator.Run();
+using var app = builder.Build();
 
-// using IHost host = CreateHostBuilder(args).Build();
-
-// var calculator = host.Services.GetService<ICalculator>();
-
-// await host.RunAsync();
-
-// static IHostBuilder CreateHostBuilder(string[] args) =>
-//     Host.CreateDefaultBuilder(args)
-//         .ConfigureServices((_, services) =>
-//             services.AddSingleton<ICalculatorService, CalculatorService>()
-//                     .AddSingleton<IDisplayMenuService, DisplayMenuService>()
-//                     .AddSingleton<ICalculator, Calculator>()
-//                     .AddScoped<IMathematicOperation, MathematicOperation>());
-
-//                     _ = host.Services.GetService<ICalculator>();
-
-// calculator.Run();
-
-// Teremos uma interface com o contrato dos metodos acima, cada metodo recebe dois valores decimais
-// Teremos uma classe que herda a interface e faz a implementação dos metodos
-// Teremos uma classe que executa a ação desejada pelo usuario
-
+app.Run();
